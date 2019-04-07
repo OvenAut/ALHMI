6696270a741d0e3b31c114499b5c6dd5a7a7c6c9
Oliver Oszko <oliver@oszko.net>
2019-04-07 20:49:24 +0200

    push
    eh what ?

---
+++ ALHMI/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ALHMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 22);
+            this.textBox1.MaxLength = 6;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(59, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.checkValue);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(8, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FehlerNr:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(73, 48);
+            this.textBox2.MaxLength = 6;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(184, 85);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ALHMI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ALHMI/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ALHMI
+{
+
+    public partial class Form1 : Form
+    {
+        private static int FNumber , modeNummer , bytesNummer;
+        private bool FNValid = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //this.textBox1.DeselectAll();
+
+            //private int modeNummer;
+            
+           if (!int.TryParse(textBox1.Text, out FNumber) && textBox1.Text != "")
+            {
+                fehlerAus("ist keine Nummmer !!");
+            } else if (textBox1.TextLength == 6)
+            {
+                if (FNumber >= 700000)
+                {
+                    if (FNumber <= 703000)
+                    {
+                        textBox2.ReadOnly = true;
+                        FNumber = FNumber - 700000;
+                        modeNummer = FNumber % 8;
+                        bytesNummer = (FNumber - modeNummer) / 8;
+                        textBox2.Text = "DB126.DBX" + bytesNummer.ToString() + '.' + modeNummer.ToString();
+                        FNValid = true;
+                        textBox2.ForeColor = Color.Green;
+                    } else
+                    {
+                        fehlerAus("Nummer > 703000");
+                    }
+                } else
+                {
+                    fehlerAus("Nummer < 700000");
+                }
+
+            } else
+            {
+                textBox2.ReadOnly = true;
+                textBox2.Text = null;
+                FNValid = false;
+                textBox2.ForeColor = Color.Black;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = HMI.DefaultNr;
+            textBox1.SelectionStart = 3;
+            textBox1.SelectionLength = 0;
+        }
+
+
+
+        private void checkValue(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (Char)Keys.Enter && FNValid)
+            {
+                Clipboard.SetText(textBox2.Text);
+            }
+            else if (e.KeyChar >='0' && e.KeyChar <= '9' || e.KeyChar == (char)Keys.Back)
+            {
+                e.Handled = false;
+            } else //if (e.KeyChar != (char)Keys.ControlKey)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void fehlerAus (string FText)
+        {
+            textBox2.ReadOnly = false;
+            FNValid = false;
+            textBox2.Text = FText;
+            textBox2.ForeColor = Color.Red;            
+        }
+    }
+}
